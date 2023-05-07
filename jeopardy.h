effb1b5bb02dc8b85ae505d7eb51f7040cbac925
GreedyCat30 <madyellak@icloud.com>
2023-05-06 19:33:41 -0700

    Jeopardy Update 5/6/23 7:33 pm

--- jeopardy.h
+++ jeopardy.h
@@ -27,12 +27,19 @@ public:
 
     Jeopardy(const string &new_question, const string &new_right_answer1, const string &new_answer2, const string &new_answer3, const string &new_answer4, const string &new_dummy) : question(new_question), right_answer1(new_right_answer1), answer2(new_answer2), answer3(new_answer3), answer4(new_answer4), dummy(new_dummy) {}
 
+	//Getters
+	string get_question() const {return question;}
+    string get_right_answer1() const {return right_answer1;}
+    string get_answer2() {return answer2;}
+    string get_answer3() const {return answer3;}
+    string get_answer4() const {return answer4;}
+	
 	friend std::istream& operator>>(std::istream& ins, Jeopardy &rhs) { //input
-		string full_question;
-    	getline(ins, full_question, '\n');
-		std::regex target("(\n)+"); //trying to replace the \n in the string with an actual endline, not working
-    	string replacement = " ";
-    	rhs.question = std::regex_replace(full_question, target, replacement);
+		//string full_question;
+    	getline(ins, rhs.question, '\n');
+		//std::regex target("(\n)+"); //trying to replace the \n in the string with an actual endline, not working
+    	//string replacement = " ";
+    	//rhs.question = std::regex_replace(full_question, target, replacement);
     	getline(ins, rhs.right_answer1, '\n');
 		getline(ins, rhs.answer2, '\n');
         getline(ins, rhs.answer3, '\n');
@@ -42,6 +49,6 @@ public:
     }
 
     friend std::ostream& operator<<(std::ostream& outs, const Jeopardy &rhs) { //output
-        return outs << rhs.question << std::endl << rhs.right_answer1 << std::endl << rhs.answer2 << std::endl << rhs.answer3 << std::endl << rhs.answer4 << std::endl <<std::endl;
+        return outs << rhs.question << std::endl <<"A. " << rhs.right_answer1 << std::endl << "B. " << rhs.answer2 << std::endl << "C. " << rhs.answer3 << std::endl << "D. "<< rhs.answer4 << std::endl <<std::endl;
     }
 };
--- main.cc
+++ main.cc
@@ -1,8 +1,18 @@
 #include<unordered_map>
 #include<fstream>
+#include<cstdlib>
+#include<time.h>
+#include<iomanip>
 #include"jeopardy.h"
+#include"/public/colors.h"
+#include"/public/read.h"
 
 using namespace std;
+
+//1. Replace string \n with actual endline
+//2. Userface
+//3. Print questions and answers randomly
+//4. A way to win the game
 
 int main(){
  Jeopardy info;
@@ -10,17 +20,38 @@ int main(){
  unordered_map<int,Jeopardy> database;
  database.reserve(1'000'000);
 
- ifstream ins("questions2.txt");
+ ifstream ins("questions.txt");
  	while(ins){
     ins>>info;
     if(!ins){break;}    
     database.insert({key,{info}});
     key++;
 }
-
-for (auto x : database){
+/*for (auto x : database){
     cout << x.second;
-}
+}*/
+
+ srand(time(0));
+ int count = 1;
+ char answer;
+ vector<int>old_questions;
+	
+ while(true){
+	int random = rand()%key;
+	cout<<"----------------------------------"<<endl;
+	cout<<"|"<<setw(20)<<"           JEOPARDY             "<<"|"<<endl;
+	cout<<"----------------------------------"<<endl;
+	cout<<database.find(random)->second<<endl;
+	old_questions.push_back(random); //stores the question numbers that have already been used
+	//need to make sure questions aren't repeated
+	
+	cout<<"Please choose an answer: "<<endl;
+	cin>>answer;
+	if(answer == 'A'){ cout<<"CORRECT!"<<RESET<<endl; } //needs to be randomized
+	else{ cout<<"WRONG"<<endl;}
+	if(count == 94) {break;} //94 questions in total
+	count++;
+	}
 
 return 0;
 }
