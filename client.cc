ea42ab1b2e04a3210c7ce97a2add42a888497112
GreedyCat30 <madyellak@icloud.com>
2023-05-09 17:46:30 -0700

    Vollyeball Bulletpoint

--- client.cc
+++ client.cc
@@ -35,22 +35,33 @@ int main(int argc, char* argv[])
 		//s >> line;
 		//Player player;
 		float new_time = 0.0, old_time = 0.0, winner = 0.0;
-		string one_time;
+		string one_time, time;
+		stringstream ss;
 		Jeopardy jeopardy;
-			cout<<setw(25)<<"****PLAYER TWO****"<<endl<<endl;
-			cout<<"----------------------------------"<<endl;
-			cout<<"|"<<setw(20)<<"           VOLLEYBALL           "<<"|"<<endl;
-			cout<<"----------------------------------"<<endl<<endl;
-			while(true){
+		cout<<setw(25)<<"****PLAYER TWO****"<<endl<<endl;
+		cout<<"----------------------------------"<<endl;
+		cout<<"|"<<setw(20)<<"           VOLLEYBALL           "<<"|"<<endl;
+		cout<<"----------------------------------"<<endl<<endl;
+		while(true){
 			cout<<endl<<"PLAYER ONE'S TURN..."<<endl;
 			getline(s,one_time);
 			if(one_time == "WRONG"){
 				cout<<"PLAYER ONE GOT THEIR QUESTION WRONG"<<endl;
-				cout<<"YOU WIN"<<endl;
+				cout<<"YOU WIN!"<<endl;
 				return 0;
 			}
-			old_time = jeopardy.get_time_to_beat(); //player one's time
+			if(one_time == "LONG"){
+				cout<<"PLAYER ONE TOOK TO LONG"<<endl;
+				cout<<"YOU WIN!"<<endl;
+				return 0;
+			}
 			cout<<one_time<<endl;
+			for(size_t i = 0; i < one_time.size(); i++){
+				one_time.erase(remove_if(one_time.begin(), one_time.end(), [](char c) { return !isdigit(c); } ), one_time.end());
+			}
+			ss << one_time;
+			ss >> old_time;
+			old_time = old_time*(0.001);
 			jeopardy.jeopardy_machine();
 			if(jeopardy.get_answer() == "WRONG"){ 
 				cout<<"PLAYER ONE WINS"<<endl;
@@ -60,12 +71,16 @@ int main(int argc, char* argv[])
 				battleship stuff	
 		 	}
 			*/
-			cout<<"Time taken to answer: "<<jeopardy.get_time_to_beat()<<" seconds"<<endl;
+			cout<<"Time taken to answer: "<<jeopardy.get_time_to_beat()<<" seconds"<<endl<<endl;
 			new_time = jeopardy.get_time_to_beat(); //plaer two's time
-			if(new_time > old_time){break;} //if player two's time is more then player one's
+			if(new_time > old_time){
+				cout<<"YOU TOOK TO LONG TO ANSWER"<<endl;
+				cout<<"PLAYER ONE WINS"<<endl;
+				s<<"LONG";
+				return 0;
+				} 
 		 	s<<"Time to beat: "<<new_time<<" seconds\n";
-		}
-	
+		}	
 	}
 
 	catch (exception& e)
